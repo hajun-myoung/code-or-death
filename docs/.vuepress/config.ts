2566e00b7513363c091a8cbf4b0e7122d37e37db
hajun <audgkwns44@gmail.com>
2022-04-10 09:11:19 +0900

    set sidebar

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -56,6 +56,10 @@ export default defineUserConfig<DefaultThemeOptions>({
         ],
       },
     ],
+
+    sidebar: {
+      "/guide/contribute/": ["README.md", "configGuide.md"],
+    },
   },
 
   plugins: [],
